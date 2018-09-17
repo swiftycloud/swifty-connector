160047c07576fd9223e1a1c094483d8a2da42bf5
Suinly <suinly@suinly.com>
2018-09-17 17:23:08 +0300

    update deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+COLOR="\033[1;34m"
+NC="\033[0m"
+
+echo -e "${COLOR}# check the repository and update the project${NC}" &&
+git pull &&
+
+echo -e "${COLOR}# rebuild the docker images${NC}" &&
+docker-compose build &&
+
+echo -e "${COLOR}# restart the containers if required${NC}" &&
+docker-compose up -d &&
+
+echo -e "${COLOR}# install or update composer dependency${NC}" &&
+docker-compose exec app composer install &&
+
+echo -e "${COLOR}# run database migrations${NC}" &&
+docker-compose exec app php artisan migrate &&
+
+echo -e "${COLOR}# install or update nodejs dependency${NC}" &&
+docker-compose exec app npm install &&
+
+echo -e "${COLOR}# build production assets${NC}" &&
+docker-compose exec app npm run prod &&
+
+echo -e "${COLOR}# all is done!${NC}"
