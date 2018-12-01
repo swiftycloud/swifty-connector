8acef85a79747edcf6a3cfe5cd9282b276dff15c
danielflow <thedanielflow@gmail.com>
2018-12-01 18:40:52 +0300

    remove http code proxy

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -133,7 +133,7 @@ class CustomerController extends Controller
             if ($e->getResponse()->getStatusCode() == 409) {
                 return response()->json(['errors' => ['email' => ['User already exists']]], 409);
             } else {
-                return response()->json([], $e->getResponse()->getStatusCode());
+                return response()->json(['status' => 'error', 'message' => 'Something was wrong, try again', 'code' => $e->getResponse()->getStatusCode()], 418);
             }
         }
     }
